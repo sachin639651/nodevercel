148b5ab94c179984384a734a5f8bd71a943c9dd7
Sachin <sachin639651@gmail.com>
2024-04-06 00:00:49 +0530

    s

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.status(200).json({"status":"working"})
 })
 
+app.get('/about', (req, res) => {
+  res.status(200).json({"status":"about us also working working","about":"developed by sachin thakur"})
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
